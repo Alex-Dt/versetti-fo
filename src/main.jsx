52fb822d95a63344ab727a27fbe36408ad069e57
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 01:35:32 +0300

    clean up project and add mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { CssBaseline } from "@mui/material";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
