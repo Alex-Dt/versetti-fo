5db5e05772b6d7ec55e251aefb7aa1aa313f742c
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 04:28:34 +0300

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,14 +1,38 @@
-import { Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { Container } from "../../components/Container";
+
+import robot from "../../assets/images/robot.png";
 
 export const Hero = () => {
   return (
-    <>
+    <Box
+      component="section"
+      sx={{
+        position: "relative",
+      }}
+    >
+      <img
+        src={robot}
+        alt="robot"
+        style={{
+          position: "absolute",
+          zIndex: -1,
+          left: -76,
+          top: "50%",
+          transform: "translateY(-50%) rotate(-7.29deg)",
+        }}
+      />
       <Container>
-        <Typography>
-          It’s not possible to reach the stars and live forever... Yet.
-        </Typography>
+        <Stack
+          alignItems={"center"}
+          minHeight={"100svh"}
+          justifyContent={"center"}
+        >
+          <Typography maxWidth={800} variant={"h1"} textAlign={"center"}>
+            It’s not possible to reach the stars and live forever... Yet.
+          </Typography>
+        </Stack>
       </Container>
-    </>
+    </Box>
   );
 };
