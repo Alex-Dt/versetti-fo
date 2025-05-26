01abb3103f62c9f310961c0a06d59ea892b987f6
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 04:28:32 +0300

    Update Exited.jsx

--- src/sections/Exited/Exited.jsx
+++ src/sections/Exited/Exited.jsx
@@ -1,3 +1,17 @@
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import { Container } from "../../components/Container";
+
 export const Exited = () => {
-  return <></>;
+  return (
+    <Box>
+      <Container>
+        <Stack mb={"128px"}>
+          <Typography variant={"h2"} mb={"8px"}>
+            Exited
+          </Typography>
+          <Grid gridTemplateColumns="repeat(5,1fr)"></Grid>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
