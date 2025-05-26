bbcbd797755d47a40ce50e43a0b3b9e0e00d1c57
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 04:28:40 +0300

    Update WhatWeDo.jsx

--- src/sections/WhatWeDo/WhatWeDo.jsx
+++ src/sections/WhatWeDo/WhatWeDo.jsx
@@ -1,3 +1,44 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { Container } from "../../components/Container";
+import { Card } from "../../components/Card";
+
+import img1 from "../../assets/images/whatwedo/1.png";
+import img2 from "../../assets/images/whatwedo/2.png";
+import img3 from "../../assets/images/whatwedo/3.png";
+
+const data = [
+  {
+    icon: img1,
+    title: "Invest",
+    text: "We prefer to be the first and lead investor in businesses we have high conviction in. We can also follow-on and bring in high value co-investors in the subsequent round.",
+  },
+  {
+    icon: img2,
+    title: "Accelerate",
+    text: "Unlike most family offices, we are led by entrepreneurs, technologists and operators, including our first-gen Founder. Our hands-on approach helps startups scale fast.",
+  },
+  {
+    icon: img3,
+    title: "Collaborate",
+    text: "Exchange of knowledge is crucial, especially given our ambitious mission. We both educate and learn from other family offices as well as governments, entrepreneurs and researchers.",
+  },
+];
+
 export const WhatWeDo = () => {
-  return <></>;
+  return (
+    <Box component={"section"}>
+      <Container>
+        <Stack mb={"87px"}>
+          <Typography mb={"61px"} variant={"h2"}>
+            What we do
+          </Typography>
+          <Stack direction={"row"} gap={"8%"}>
+            {data.map((_data) => {
+              return <Card data={_data} />;
+            })}
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
