e12ebcf91a7171399eaba27af6f20f0a6ab6aa9c
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 04:28:38 +0300

    Update Verticals.jsx

--- src/sections/Verticals/Verticals.jsx
+++ src/sections/Verticals/Verticals.jsx
@@ -1,3 +1,40 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { Card } from "../../components/Card";
+
+import img1 from "../../assets/images/verticals/1.png";
+import img2 from "../../assets/images/verticals/2.png";
+import img3 from "../../assets/images/verticals/3.png";
+
+const data = [
+  {
+    title: "Longevity",
+    icon: img1,
+  },
+  {
+    title: "Artificial Intelligence",
+    icon: img2,
+  },
+  {
+    title: "Web3",
+    icon: img3,
+  },
+];
+
 export const Verticals = () => {
-  return <></>;
+  return (
+    <Box component={"section"}>
+      <Container>
+        <Stack mb={"125px"}>
+          <Typography mb={"35px"} variant={"h2"}>
+            Verticals
+          </Typography>
+          <Stack direction={"row"} gap={2}>
+            {data.map((_data) => {
+              return <Card type={3} data={_data} />;
+            })}
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
