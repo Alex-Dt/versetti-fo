b2e980021ee179db45be7318c7ba849b7a19b2ad
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 04:28:36 +0300

    Update Portfolio.jsx

--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -1,3 +1,32 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { Card } from "../../components/Card";
+import { SolanaLogo } from "../../assets/images/portfolios/solanaLogo";
+import { SonicLogo } from "../../assets/images/portfolios/sonicLogo";
+
+const data = [
+  {
+    icon: <SolanaLogo height={62} />,
+  },
+  {
+    icon: <SonicLogo height={103} />,
+  },
+];
+
 export const Portfolio = () => {
-  return <></>;
+  return (
+    <Box component={"section"}>
+      <Container>
+        <Stack mb={"51px"}>
+          <Typography mb={"24px"} variant={"h2"}>
+            Portfolio
+          </Typography>
+          <Stack direction={"row"} gap={2}>
+            {data.map((_data) => {
+              return <Card type={2} data={_data} />;
+            })}
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
