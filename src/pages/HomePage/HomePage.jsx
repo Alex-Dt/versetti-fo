2cb3c016013bdb85ad05cb9bc636862e00e15c23
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-05-26 15:32:47 +0300

    add id for founder section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,49 +4,51 @@ import { Verticals } from "../../sections/Verticals";
 import { Portfolio } from "../../sections/Portfolio";
 import { Exited } from "../../sections/Exited";
 import { Container } from "../../components/Container";
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
-      <Container>
-        <Stack
-          gap={3}
-          sx={{
-            maxWidth: 1200,
-            textAlign: {
-              xs: "center",
-              sm: "left",
-            },
-            pt: {
-              xs: "0px",
-              sm: "126px",
-            },
-            pb: {
-              xs: "56px",
-              sm: "86px",
-            },
-          }}
-        >
-          <Typography fontSize={22}>
-            Founded by the serial entrepreneur Angel Versetti, our Family Office
-            seeks out the boldest deep tech ventures and nurtures them with
-            funding, strategic guidance and operational support.
-          </Typography>
+      <Box comonent="section" id="founder">
+        <Container>
+          <Stack
+            gap={3}
+            sx={{
+              maxWidth: 1200,
+              textAlign: {
+                xs: "center",
+                sm: "left",
+              },
+              pt: {
+                xs: "0px",
+                sm: "126px",
+              },
+              pb: {
+                xs: "56px",
+                sm: "86px",
+              },
+            }}
+          >
+            <Typography fontSize={22}>
+              Founded by the serial entrepreneur Angel Versetti, our Family
+              Office seeks out the boldest deep tech ventures and nurtures them
+              with funding, strategic guidance and operational support.
+            </Typography>
 
-          <Typography fontSize={22}>
-            In particular, we focus on longevity startups and R&D projects that
-            serve as pieces of the puzzle to solve aging and increase human
-            lifespan and healthspan.
-          </Typography>
+            <Typography fontSize={22}>
+              In particular, we focus on longevity startups and R&D projects
+              that serve as pieces of the puzzle to solve aging and increase
+              human lifespan and healthspan.
+            </Typography>
 
-          <Typography fontSize={22}>
-            So that ultimately, we could, indeed, reach the stars and live
-            forever.
-          </Typography>
-        </Stack>
-      </Container>
+            <Typography fontSize={22}>
+              So that ultimately, we could, indeed, reach the stars and live
+              forever.
+            </Typography>
+          </Stack>
+        </Container>
+      </Box>
       <WhatWeDo />
       <Verticals />
       <Portfolio />
