41562b2c949d7129c5045f3c6aa27884af21f386
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-08-20 15:50:17 +0300

    Update jobs

--- src/data/jobs/index.jsx
+++ src/data/jobs/index.jsx
@@ -2,38 +2,39 @@ import { List, ListItem, Typography } from "@mui/material";
 
 export const jobsData = [
   {
-    title: "Business Analyst",
-    location: "Greater Delhi Area",
+    title: "Chief Scientific Officer",
+    location: "Dubai, London, Moscow",
     types: ["Remote", "Full-time"],
-    linkedIn:
-      "https://www.linkedin.com/jobs/search/?currentJobId=4285827677&f_C=9349071&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=4285830556%2C4285824990%2C4285824975%2C4285832381%2C4285830479%2C4285827677",
+    linkedIn: "https://www.linkedin.com/jobs/view/4284768400/",
     content: (
       <>
         <Typography variant={"h3"}>Role Description</Typography>
         <Typography>
-          This is a full-time remote role for a Business Analyst. The Business Analyst will be responsible for analyzing business processes, identifying business requirements, and providing strategic
-          recommendations. Daily tasks include conducting business analysis, preparing detailed reports, and collaborating with various teams to implement effective business solutions. The role
-          requires strong analytical skills and effective communication to support the organization's goals.
+          This is a full-time remote role for a Chief Scientific Officer (Longevity). The Chief Scientific Officer will be responsible for overseeing the research and development processes, leading
+          scientific research initiatives, managing laboratory activities, and coordinating with medical and scientific professionals. The role involves strategic planning, guiding the scientific
+          direction of projects, and ensuring the alignment of research outcomes with business goals. CSO will also help run technical DD on startups and projects and validate them, tracking their
+          scientific/technological progress.
         </Typography>
         <Typography variant={"h3"}>Qualifications</Typography>
         <List>
-          <ListItem>Analytical Skills and Business Analysis experience</ListItem>
-          <ListItem>Expertise in Business Process and identifying Business Requirements</ListItem>
-          <ListItem>Strong Communication skills</ListItem>
-          <ListItem>Excellent problem-solving abilities and attention to detail</ListItem>
-          <ListItem>Ability to work independently and remotely</ListItem>
-          <ListItem>Experience in the technology or healthcare industry is a plus</ListItem>
-          <ListItem>Bachelor's degree in Business Administration, Information Technology, or a related field</ListItem>
+          <ListItem>Lifelong passion about longevity, as demonstrated by professional and academic background</ListItem>
+          <ListItem>Research and Development (R&D) and Research skills</ListItem>
+          <ListItem>Science and Medicine proficiency</ListItem>
+          <ListItem>Laboratory Skills</ListItem>
+          <ListItem>Strong leadership and management abilities</ListItem>
+          <ListItem>Excellent communication and collaboration skills</ListItem>
+          <ListItem>Strategic thinking and problem-solving capabilities</ListItem>
+          <ListItem>Experience in the longevity industry is a plus</ListItem>
+          <ListItem>Ph.D. or M.D. in relevant scientific disciplines</ListItem>
         </List>
       </>
     ),
   },
   {
-    title: "Virtual Assistant",
-    location: "Greater Bengaluru Area",
+    title: "Public Relations Manager",
+    location: "Dubai, London, Moscow",
     types: ["Remote", "Full-time"],
-    linkedIn:
-      "https://www.linkedin.com/jobs/search/?currentJobId=4285824975&f_C=9349071&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=4285830556%2C4285824990%2C4285824975%2C4285832381%2C4285830479%2C4285827677",
+    linkedIn: "https://www.linkedin.com/jobs/view/4284767534/",
     content: (
       <>
         <Typography variant={"h3"}>Role Description</Typography>
@@ -42,16 +43,30 @@ export const jobsData = [
           correspondence, and performing research. In addition, the role includes data entry, document management, and providing general support to executives and team members. The ideal candidate
           will be highly organized, capable of multitasking, and comfortable working independently in a virtual environment.
         </Typography>
+        <Typography variant={"h3"}>Specific responsibilities</Typography>
+        <List>
+          <ListItem>Develop personal brand and PR strategy for the Founder</ListItem>
+          <ListItem>
+            Media and speaking opportunities. Reaching out to existing, and seeking out new, media outlets and events to profile the Founder as thought leader or secure a speaking opportunity
+          </ListItem>
+          <ListItem>Drafting of documents based on key bulletpoints</ListItem>
+          <ListItem>Negotiating any commercial deals with the advantage to the founder</ListItem>
+          <ListItem>Other administrative or logistical tasks related to the broader operations of the fund and its projects may be given at any point of time</ListItem>
+        </List>
         <Typography variant={"h3"}>Qualifications</Typography>
         <List>
-          <ListItem>Experience in administrative tasks including managing schedules, coordinating meetings, and handling correspondence</ListItem>
-          <ListItem>Strong organizational and multitasking skills</ListItem>
-          <ListItem>Proficiency in data entry and document management</ListItem>
-          <ListItem>Ability to conduct research and provide general support to team members</ListItem>
-          <ListItem>Excellent written and verbal communication skills</ListItem>
-          <ListItem>High level of comfort working independently and remotely</ListItem>
-          <ListItem>Familiarity with virtual communication and collaboration tools such as Slack, Zoom, and Google Workspace</ListItem>
-          <ListItem>Prior experience in a similar role is a plus</ListItem>
+          <ListItem>Network and Charisma</ListItem>
+          <ListItem>Strong communication skills</ListItem>
+          <ListItem>Existing relationships that would permit to get relevant exposure and opportunities with more economic efficiency</ListItem>
+          <ListItem>Perfect English is a must. Working/native knowledge of other major global languages is a plus</ListItem>
+          <ListItem>Experience working with a team in a non-corporate environment. We do NOT have various standard corporate processes in place!</ListItem>
+        </List>
+        <Typography variant={"h3"}>Benefits</Typography>
+        <List>
+          <ListItem>A competitive remuneration</ListItem>
+          <ListItem>In case of travel to events, budgets to cover basic fare and accommodation costs</ListItem>
+          <ListItem>Access to high profile networks and invitation-only events, where appropriate</ListItem>
+          <ListItem>Opportunity to transition to higher responsibility positions after proven results and loyalty</ListItem>
         </List>
       </>
     ),
