bebb3f63ee69f3e7e240110c0ac3f224176331b8
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-08-20 15:11:29 +0300

    Fix path

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,5 +1,14 @@
-import { Box, Button, Card, Chip, List, ListItem, Stack, Typography } from "@mui/material";
-import { Bag } from "../../assets/images/jobs/bag";
+import {
+  Box,
+  Button,
+  Card,
+  Chip,
+  List,
+  ListItem,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Bag } from "../../assets/images/jobs/Bag";
 import { Container } from "../../components/Container";
 import { JobsCard } from "../../components/JobsCard";
 import { jobsData } from "../../data/jobs";
@@ -78,9 +87,13 @@ export const Jobs = () => {
                   textAlign: "justify",
                 }}
               >
-                Versetti Family Office, founded by serial entrepreneur Angel Versetti, focuses on nurturing bold deep-tech ventures with funding, strategic guidance, and operational support. We
-                prioritize longevity startups and R&D projects aimed at solving aging to increase human lifespan and healthspan. Our mission is to ultimately help humanity reach new heights and
-                achieve longer, healthier lives.
+                Versetti Family Office, founded by serial entrepreneur Angel
+                Versetti, focuses on nurturing bold deep-tech ventures with
+                funding, strategic guidance, and operational support. We
+                prioritize longevity startups and R&D projects aimed at solving
+                aging to increase human lifespan and healthspan. Our mission is
+                to ultimately help humanity reach new heights and achieve
+                longer, healthier lives.
               </Typography>
               <Stack
                 sx={{
